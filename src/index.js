bb345de7acb5ce7a2f01b22824e477eb0afc8e61
Dromediansk <pillar.mr@gmail.com>
2023-09-18 11:07:25 +0200

    Initial setup with ping command

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+
+const { clientReadyHandler } = require("./events/clientReady");
+const { interactionCreateHandler } = require("./events/interactionCreate");
+
+const pingCommand = require("./commands/ping");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.commands = new Collection();
+
+client.commands.set(pingCommand.data.name, pingCommand);
+
+client.once(Events.ClientReady, clientReadyHandler);
+
+client.on(Events.InteractionCreate, interactionCreateHandler);
+
+client.login(process.env.DISCORD_TOKEN);
